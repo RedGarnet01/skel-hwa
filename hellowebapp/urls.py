d7b962eac5cef58d615679acc0c7b3458bd1a55e
Jovan Dandridge <jovandandridge@Jovans-MacBook-Pro.local>
2018-10-13 00:06:47 -0700

    Updated the templates folder, as well as views, settings, and urls

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import url
 
+from django.views.generic import TemplateView
+
 from collection import views
 
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
+    url(r'^about/$',TemplateView.as_view(template_name='about.html'), name='about'),
+    url(r'^contact/$',TemplateView.as_view(template_name='contact.html'), name='contact'),
     url('admin/', admin.site.urls),
 ]
