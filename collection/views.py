ac999761936f136a0f0f35855fa39d19b446b935
Jovan Dandridge <jovandandridge@Jovans-MacBook-Pro.local>
2018-10-13 01:52:14 -0700

    updated index and views to work with the database

--- collection/views.py
+++ collection/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+from collection.models import Thing
 
 
+# def index(request):
+#     number = 6
+#     # don't forget the quotes because it's a string, not an integer
+#     thing = "Thing name"
+#     return render(request, 'index.html', {
+#         'number': number,
+#         # don't forget to pass it in, and the last comma
+#         'thing': thing,
+#     })
+
+# the rewritten view!
 def index(request):
-    number = 6
-    # don't forget the quotes because it's a string, not an integer
-    thing = "Thing name"
+    things = Thing.objects.all()
     return render(request, 'index.html', {
-        'number': number,
-        # don't forget to pass it in, and the last comma
-        'thing': thing,
+        'things': things,
     })
